31f3c3a6da25d87cc5a507846d8ac228a5a41ed4
Penezuel <riansuryaa98@gmail.com>
2022-07-12 11:11:49 +0700

    first commit2

---
+++ application/views/home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bgimg {
+  background-image: url('https://firebasestorage.googleapis.com/v0/b/kasver-92d84.appspot.com/o/pexels-felix-mittermeier-956999.jpg?alt=media&token=71102362-c721-42e8-b5b6-5e72354765c3');
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: white;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 25px;
+}
+
+.topleft {
+  position: absolute;
+  top: 0;
+  left: 16px;
+}
+
+.bottomleft {
+  position: absolute;
+  bottom: 0;
+  left: 16px;
+}
+
+.middle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+hr {
+  margin: auto;
+  width: 40%;
+}
+</style>
+<body>
+
+<div class="bgimg">
+  <div class="topleft">
+    <img src="https://firebasestorage.googleapis.com/v0/b/kasver-92d84.appspot.com/o/logo_round.png?alt=media&token=421b8fdd-10c8-4c6e-8c56-ad8669112385" width="50" height="50">
+  </div>
+  <div class="middle">
+  <img src="https://firebasestorage.googleapis.com/v0/b/kasver-92d84.appspot.com/o/logo_round.png?alt=media&token=421b8fdd-10c8-4c6e-8c56-ad8669112385" alt="KasVer" width="350" height="350">
+    <h1>COMING SOON</h1>
+    <hr>
+    <p>Aplikasi POS (Point Of Sales) online berbasis cloud yang dapat membawa potensi usaha Anda ke level tertinggi, baik online maupun offline.</p>
+  </div>
+  <div class="bottomleft">
+    <p>Sedang dalam pengembangan...</p>
+  </div>
+</div>
+
+</body>
+</html>
